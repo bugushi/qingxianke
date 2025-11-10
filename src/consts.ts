9a6469dcfa4b4f3af303ad54ca29113b940bb6c0
bugu <bugushi@gmail.com>
2025-11-10 22:22:28 +0800

    change name

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '布谷';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '清闲客';
+export const SITE_DESCRIPTION = '浮生一梦';
